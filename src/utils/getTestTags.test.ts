e1e70b2476bdf606b86088a6b9097b7bab669e2e
Elio Struyf <elio@struyfconsulting.be>
2024-03-08 21:50:43 +0100

    Test coverage

--- src/utils/getTestTags.test.ts
+++ src/utils/getTestTags.test.ts
@@ -1,6 +1,12 @@
 import { getTestTags } from "./getTestTags";
 
 describe("getTestTags", () => {
+  it("should return empty string test is not defined", () => {
+    const result = getTestTags(undefined as any);
+
+    expect(result).toBe("");
+  });
+
   it("should return empty string when test tags are undefined", () => {
     const test = {
       tags: undefined,
@@ -24,6 +30,16 @@ describe("getTestTags", () => {
   it("should return tags without '@' symbol", () => {
     const test = {
       tags: ["@tag1", "@tag2", "@tag3"],
+    };
+
+    const result = getTestTags(test as any);
+
+    expect(result).toBe("tag1, tag2, tag3");
+  });
+
+  it("should return tags even when they don't start with '@' symbol", () => {
+    const test = {
+      tags: ["tag1", "tag2", "tag3"],
     };
 
     const result = getTestTags(test as any);
