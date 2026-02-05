c5c541c9a27a303dbe6562f608ebe564c4766df4
Elio Struyf <elio@struyfconsulting.be>
2026-02-05 17:12:36 +0100

    Refactor update-package-version script to use ES modules and improve path handling

--- scripts/update-package-version.js
+++ scripts/update-package-version.js
@@ -1,6 +1,13 @@
-const fs = require('fs');
-const path = require('path');
-const packageJson = require('../package.json');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const packageJsonPath = path.join(__dirname, '../package.json');
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+
 packageJson.version += `-beta.${process.argv[process.argv.length - 1].substr(0, 7)}`;
 console.log(packageJson.version);
-fs.writeFileSync(path.join(path.resolve('.'), 'package.json'), JSON.stringify(packageJson, null, 2));
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
